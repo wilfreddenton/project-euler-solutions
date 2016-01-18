4cd84138a70a8f04361bbd3e8a815a6dc483670d
Wilfred Denton <dentonw3@gmail.com>
2016-01-17 19:47:56 -0500

    solved 18 and 67 as they are part 1 and 2 of the same problem

---
+++ 1-50/018_maximum_path_sum_1.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var path = require('path');
+
+function maximumPathSum(tree) {
+  for (var i = tree.length - 2; i >= 0; i -= 1) {
+    for (var j = 0; j <= tree[i].length - 1; j += 1) {
+      if (tree[i + 1][j] > tree[i + 1][j + 1])
+        tree[i][j] += tree[i + 1][j];
+      else
+        tree[i][j] += tree[i + 1][j + 1];
+    }
+  }
+  return tree[0][0];
+}
+
+var tree = fs.readFileSync(path.join(__dirname, 'data/018_triangle.txt'), 'utf8');
+tree = tree.split('\n').slice(0, -1).map(function(row) {
+  return row.split(' ').map(function(num) {
+    return parseInt(num);
+  });
+});
+
+console.log(maximumPathSum(tree));
