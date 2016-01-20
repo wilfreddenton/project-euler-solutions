175517309913817660033859ac87d9fbbe2b896e
Wilfred Denton <dentonw3@gmail.com>
2016-01-20 17:59:25 -0500

    finished hardest problem yet. #24 lexicographic permutations

--- 1-50/024_lexicographic_permutations.js
+++ 1-50/024_lexicographic_permutations.js
@@ -1,8 +1,51 @@
 var list = require('../utils').list;
 
-function lexicographicPermutations(upperBound) {
-  var nums = list(upperBound + 1)
-  return nums;
+function nonIncreasingSuffix(perm) {
+  for (var i = perm.length - 1; i > 0; i -= 1) {
+    if (perm[i - 1] < perm[i]) {
+      return i;
+    }
+  }
+}
+function rightMostSuccessor(perm, pivotIndex, suffixStart) {
+  var successorIndex = suffixStart;
+  var successor = perm[suffixStart];
+  var pivot = perm[pivotIndex];
+  for (var i = suffixStart + 1; i <= perm.length; i += 1) {
+    var num = perm[i];
+    if (num > pivot && num <= successor) {
+      successorIndex = i;
+      successor = num;
+    }
+  }
+  return successorIndex;
+}
+function reverse(arr) {
+  return arr.length === 0 ? [] : reverse(arr.slice(1)).concat(arr[0]);
+}
+function nextPermutation(perm) {
+  perm = perm.slice();
+  var suffixStart = nonIncreasingSuffix(perm);
+  var pivotIndex = suffixStart - 1;
+  var rightMostSuccessorIndex = rightMostSuccessor(perm, pivotIndex, suffixStart);
+  var pivot = perm[pivotIndex];
+  perm[pivotIndex] = perm[rightMostSuccessorIndex];
+  perm[rightMostSuccessorIndex] = pivot;
+  var reversedSuffix = reverse(perm.slice(suffixStart));
+  perm = perm.slice(0, suffixStart).concat(reversedSuffix);
+  return perm;
+}
+function lexicographicPermutations(n, upperBound) {
+  var start = list(n + 1);
+  var perms = [start];
+  for (var i = 1; i < upperBound; i += 1) {
+    perms.push(nextPermutation(perms[i - 1]))
+  }
+  return perms;
+}
+function lexicographicPermutation(n, upperBound) {
+  var perms = lexicographicPermutations(n, upperBound);
+  return perms[upperBound - 1].join('');
 }
 
-console.log(lexicographicPermutations(9));
+console.log(lexicographicPermutation(9, 1000000));
